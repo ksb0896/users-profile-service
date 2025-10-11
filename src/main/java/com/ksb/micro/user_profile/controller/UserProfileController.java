7b5d4f9ece69d6cac7d4bfc1270aca7f38931053
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-10-11 15:13:04 +0530

    user profile 4 apis -done

--- src/main/java/com/ksb/micro/user_profile/controller/UserProfileController.java
+++ src/main/java/com/ksb/micro/user_profile/controller/UserProfileController.java
@@ -1,4 +1,40 @@
 package com.ksb.micro.user_profile.controller;
 
+
+import com.ksb.micro.user_profile.model.UserProfile;
+import com.ksb.micro.user_profile.service.impl.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/banks/{bankId}/users")
 public class UserProfileController {
+
+    @Autowired
+    private UserProfileService userProfileService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserProfile> getUserProfile(@PathVariable Long bankId, @PathVariable Long userId){
+        return userProfileService.getUserProfile(bankId, userId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserProfile> createUserProfile(@PathVariable Long bankId, @RequestBody UserProfile userProfile){
+        userProfile.setBankId(bankId);
+        UserProfile createUser = userProfileService.createUserProfile(userProfile);
+        return ResponseEntity.ok(createUser);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserProfile> updateUserProfile(@PathVariable Long bankId, @PathVariable Long userId, @RequestBody UserProfile userProfile){
+        UserProfile updateUser = userProfileService.updateUserProfile(bankId,userId,userProfile);
+        return updateUser !=null ? ResponseEntity.ok(updateUser) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<UserProfile> deleteUserProfile(@PathVariable Long bankId, @PathVariable Long userId){
+        userProfileService.deleteUserProfile(bankId,userId);
+        return ResponseEntity.noContent().build();
+    }
 }
