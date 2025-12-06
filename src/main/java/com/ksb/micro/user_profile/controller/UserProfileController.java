bd4f7e18a37f06f3f5e8d82dbb2693331604c47c
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-12-06 21:47:54 +0530

    migrated to webmvc(not working)

--- src/main/java/com/ksb/micro/user_profile/controller/UserProfileController.java
+++ src/main/java/com/ksb/micro/user_profile/controller/UserProfileController.java
@@ -2,6 +2,9 @@ package com.ksb.micro.user_profile.controller;
 
 import com.ksb.micro.user_profile.model.UserProfile;
 import com.ksb.micro.user_profile.service.impl.UserProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +15,25 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/banks/{bankId}/users")
+@Tag(name = "Users", description = "User Management")
 public class UserProfileController {
 
     @Autowired
     private UserProfileService userProfileService;
 
     @GetMapping("/{userId}") //by user ID
+    @Operation(summary = "Get user by ID")
+    @ApiResponse(responseCode = "200", description = "User found")
+    @ApiResponse(responseCode = "404", description = "User not found")
     public ResponseEntity<UserProfile> getUserProfile(@PathVariable Long bankId, @PathVariable Long userId){
         UserProfile profile = userProfileService.getUserProfile(bankId, userId);
         return ResponseEntity.ok(profile);
     }
 
     @GetMapping
+    @Operation(summary = "Get ALL users")
+    @ApiResponse(responseCode = "200", description = "User found")
+    @ApiResponse(responseCode = "404", description = "User not found")
     public ResponseEntity<List<UserProfile>> getAllUserProfiles(@PathVariable Long bankId){
         List<UserProfile> profiles = userProfileService.getAllUserProfiles(bankId);
         if (profiles.isEmpty()){
@@ -33,6 +43,8 @@ public class UserProfileController {
     }
 
     @PostMapping
+    @Operation(summary = "User created")
+    @ApiResponse(responseCode = "201", description = "User created with ID")
     public ResponseEntity<UserProfile> createUserProfile(@PathVariable Long bankId, @RequestBody UserProfile userProfile){
         userProfile.setBankId(bankId);
         UserProfile createdProfile = userProfileService.createUserProfile(userProfile);
@@ -40,12 +52,14 @@ public class UserProfileController {
     }
 
     @PutMapping("/{userId}")
+    @Operation(summary = "User updated")
     public ResponseEntity<? extends Object> updateUserProfile(@PathVariable Long bankId, @PathVariable Long userId, @RequestBody UserProfile userProfile){
         UserProfile updateUser = userProfileService.updateUserProfile(bankId,userId,userProfile);
         return ResponseEntity.ok(updateUser);
     }
 
     @DeleteMapping("/{userId}")
+    @Operation(summary = "User deleted")
     public ResponseEntity<UserProfile> deleteUserProfile(@PathVariable Long bankId, @PathVariable Long userId){
         userProfileService.deleteUserProfile(bankId,userId);
         return ResponseEntity.noContent().build();
